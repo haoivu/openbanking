62fc34d0bec13b039de39c64a767416e02d4d805
haoivu <haoivu@hotmail.com>
2018-03-22 13:38:23 +0100

    oauth2 beginning

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Runtime.Serialization.Json;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace openbanking
 {
 
     public class Program
     {
-        private static readonly HttpClient client = new HttpClient();
-
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
